7f259d0f36229749eb533d6baac396843aab6c37
joel alexander <wksx9d@gmail.com>
2025-04-15 23:01:42 -0500

    mejoras de estilo a LazyColumn

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,12 +45,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LazyColumnEjem() {
     LazyColumn (
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentPadding = PaddingValues(20.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ){
         items(10){ index ->
-            Text(text = "Item $index")
+            Text(
+                text = "Item $index",
+                fontSize = 18.sp,
+                color = Color.Blue,
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .padding(8.dp)
+
+                )
         }
 
     }
@@ -97,7 +108,7 @@ fun GridEjem(){
 @Composable
 fun GreetingPreview3() {
     Lab04Theme {
-        GridEjem()
+        LazyColumnEjem()
     }
 }
 
@@ -105,7 +116,7 @@ fun GreetingPreview3() {
 @Composable
 fun GreetingPreview() {
     Lab04Theme {
-        LazyColumnEjem()
+        LazyRowEjem()
     }
 }
 
@@ -113,6 +124,6 @@ fun GreetingPreview() {
 @Composable
 fun GreetingPreview2() {
     Lab04Theme {
-        LazyRowEjem()
+        GridEjem()
     }
 }
